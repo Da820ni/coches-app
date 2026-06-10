8c87dce2aa3da7eda9c2adee81086c88d419cf7a
Da820ni <lupsadaniel16@gmail.com>
2026-06-10 15:59:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ const ASSETS = [
   '/Buscador_coches_cliente.html',
   '/Comparador_coches_DE_ES.html',
   '/manifest.webmanifest',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/icons/apple-touch-icon.png'
+  '/icon-192.png',
+  '/icon-512.png',
+  '/apple-touch-icon.png'
 ];
 
 self.addEventListener('install', e => {
